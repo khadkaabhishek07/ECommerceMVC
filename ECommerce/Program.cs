425d464b7ca665db08bf96f4a34b0dbcd3f49086
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2025-03-07 21:17:44 +0545

    conn_string

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
