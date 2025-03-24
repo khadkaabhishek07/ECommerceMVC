20c8f266d2f144161dc27c21a1ef8da71b725cc7
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2025-03-24 10:29:48 +0545

    landing page design

--- ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54fb05e8df81d55cc2dfd164d4a34fda76a950a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e21602206a5c4d1ef591dcc5836224c83e3b1d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
