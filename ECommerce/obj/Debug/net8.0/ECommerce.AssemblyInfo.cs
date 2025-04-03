450a6f7bcf2123e72ba0464573e14596db0145fe
khadkaabhishek07 <khadkaabhishek749@gmail.com>
2025-04-03 22:22:53 +0545

    conn string update

--- ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
+++ ECommerce/obj/Debug/net8.0/ECommerce.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c8f266d2f144161dc27c21a1ef8da71b725cc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22153022a56148dadeb787f02a2cec333108fc48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
